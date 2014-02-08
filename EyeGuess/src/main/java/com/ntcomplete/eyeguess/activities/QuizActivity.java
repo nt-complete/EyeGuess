ae579b94a8a84ee651f2056f67e2f4c833a140f3
Nick Tiller <ntiller121@gmail.com>
2014-02-08 02:12:16 -0600

    Added simple tap to mark question correct functionality

--- EyeGuess/src/main/java/com/ntcomplete/eyeguess/activities/QuizActivity.java
+++ EyeGuess/src/main/java/com/ntcomplete/eyeguess/activities/QuizActivity.java
@@ -2,7 +2,9 @@ package com.ntcomplete.eyeguess.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 import com.ntcomplete.eyeguess.R;
@@ -17,6 +19,13 @@ public class QuizActivity extends Activity {
 
     private GestureDetector mGestureDetector;
 
+    private TextView mQuestionView;
+    private TextView mScoreView;
+
+    private int mScore = 0;
+
+    private JSONHelper mJSONHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +33,13 @@ public class QuizActivity extends Activity {
 
         int categoryId = getIntent().getExtras().getInt(EXTRA_QUIZ_CATEGORY);
 
-        TextView questionView = (TextView) findViewById(R.id.activity_quiz_question);
+        mQuestionView = (TextView) findViewById(R.id.activity_quiz_question);
+        mScoreView = (TextView) findViewById(R.id.activity_quiz_score);
+        mScoreView.setText(String.valueOf(mScore));
 
-        JSONHelper jsonHelper = new JSONHelper(this, categoryId);
+        mJSONHelper = new JSONHelper(this, categoryId);
 
-        questionView.setText(jsonHelper.getQuestion());
+        mQuestionView.setText(mJSONHelper.getQuestion());
 
         mGestureDetector = new GestureDetector(this);
         mGestureDetector.setBaseListener(mBaseListener);
@@ -37,9 +48,25 @@ public class QuizActivity extends Activity {
     GestureDetector.BaseListener mBaseListener = new GestureDetector.BaseListener() {
         @Override
         public boolean onGesture(Gesture gesture) {
+            if(gesture == Gesture.TAP) {
+                Toast.makeText(QuizActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
+                mScore++;
+                mScoreView.setText(String.valueOf(mScore));
 
+                mQuestionView.setText(mJSONHelper.getQuestion());
+                return true;
+            }
             
             return false;
         }
     };
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
+    }
 }
