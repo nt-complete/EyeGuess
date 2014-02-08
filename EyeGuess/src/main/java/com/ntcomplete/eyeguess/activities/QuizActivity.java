94f88a3e3d5cd3f9baac790daa51b238d0e79737
Nick Tiller <ntiller121@gmail.com>
2014-02-08 01:48:13 -0600

    Added GestureDetector

--- EyeGuess/src/main/java/com/ntcomplete/eyeguess/activities/QuizActivity.java
+++ EyeGuess/src/main/java/com/ntcomplete/eyeguess/activities/QuizActivity.java
@@ -3,6 +3,8 @@ package com.ntcomplete.eyeguess.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 import com.ntcomplete.eyeguess.R;
 import com.ntcomplete.eyeguess.helpers.JSONHelper;
 
@@ -12,6 +14,8 @@ import com.ntcomplete.eyeguess.helpers.JSONHelper;
 public class QuizActivity extends Activity {
 
     public static final String EXTRA_QUIZ_CATEGORY = "QuizCategory";
+
+    private GestureDetector mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,17 @@ public class QuizActivity extends Activity {
         JSONHelper jsonHelper = new JSONHelper(this, categoryId);
 
         questionView.setText(jsonHelper.getQuestion());
+
+        mGestureDetector = new GestureDetector(this);
+        mGestureDetector.setBaseListener(mBaseListener);
     }
+
+    GestureDetector.BaseListener mBaseListener = new GestureDetector.BaseListener() {
+        @Override
+        public boolean onGesture(Gesture gesture) {
+
+            
+            return false;
+        }
+    };
 }
